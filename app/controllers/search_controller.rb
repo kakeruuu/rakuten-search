45ed451b08d322833303efe4054a37644915043e
suzuki kakeru <kakeruuuuuu0114.2@gmail.com>
2020-08-19 01:09:33 +0000

    create search controller

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,22 @@
+class SearchController < ApplicationController
+  require 'net/http'
+  API_BASE_URL='https://app.rakuten.co.jp/services/api/BooksBook/Search/20170404'
+  
+  def search
+    if keyword = params[:keyword]
+      params = {
+        'title'         => URI.encode(keyword),
+        'format'        => 'json',
+        'applicationId' => '1005768067187627415',
+        'booksGenreId'  => '001',
+        'hits'          => '20',
+        'sort'          => 'sales'
+      }
+      uri = URI(API_BASE_URL + '?' + params.map{|k,v| "#{k}=#{v}"}.join('&'))
+      response_json = Net::HTTP.get(uri)
+      response_data = JSON.parse(response_json)
+      @item = response_data["Items"]
+    end
+  end
+  
+end
